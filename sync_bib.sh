11ae1a55244ef6c3b91f9b921d1d69129f5d2e64
Everett Schlawin <granfalloonToyballoon@hotmail.com>
2020-12-02 09:48:26 -0700

    updating the path to master bib file

--- sync_bib.sh
+++ sync_bib.sh
@@ -1,1 +1,1 @@
-cp /Users/everettschlawin/IDrive-Sync/evmemo_folder/pdfs/master_bib_file/master_biblio.bib this_biblio.bib
+cp /Users/everettschlawin/Documents/evmemo_folder/pdfs/master_bib_file/master_biblio.bib this_biblio.bib
